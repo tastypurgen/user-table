f26ec4dbf4dbd64f22a1bf1ace2619b9b67fb4ba
Andrei Abramchuk <stilegs@gmail.com>
2020-08-13 13:14:20 +0300

    3/7

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+const path = require('path');
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'task4',
-})
-
-db.connect((err, res) => {
-    if (err) console.log(err)
-    else console.log()
-})
-
-app.get('/', (req, res) => {
-    res.send('<h1>table</h1>');
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+db.connect((err) => {
+  if (err) console.log(err);
+  else console.log('MySql connected');
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(process.env.PORT || 5500, () => {
-    console.log('Server started...');
+  console.log('Server started...');
 });
