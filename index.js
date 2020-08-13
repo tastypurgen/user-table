cdc4dc33ce40e2ecfa5c4589a036e73d70ccc91f
Andrei Abramchuk <stilegs@gmail.com>
2020-08-13 12:55:42 +0300

    2/7

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'task4',
+})
+
+db.connect((err, res) => {
+    if (err) console.log(err)
+    else console.log()
+})
 
 app.get('/', (req, res) => {
     res.send('<h1>table</h1>');
